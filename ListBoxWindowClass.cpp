3bac384081e186def2220773770ba4727a53154f
jps1973 <47426983+jps1973@users.noreply.github.com>
2024-12-26 09:37:29 +0000

    Added List Box Window Class

---
+++ ListBoxWindowClass.cpp
@@ -0,0 +1,168 @@
+// ListBoxWindowClass.cpp
+
+#include "ListBoxWindowClass.h"
+
+ListBoxWindow::ListBoxWindow()
+{
+} // End of function ListBoxWindow::ListBoxWindow
+ 
+ListBoxWindow::~ListBoxWindow()
+{
+} // End of function ListBoxWindow::~ListBoxWindow
+
+int ListBoxWindow::AddString( LPCTSTR lpszString )
+{
+	// Add string
+	return ::SendMessage( m_hWnd, LB_ADDSTRING, ( WPARAM )NULL, ( LPARAM )lpszString );
+
+} // End of function ListBoxWindow::AddString
+
+int ListBoxWindow::AddText( LPCTSTR lpszText )
+{
+	// Add text
+	return ::SendMessage( m_hWnd, LB_ADDSTRING, ( WPARAM )NULL, ( LPARAM )lpszText );
+
+} // End of function ListBoxWindow::AddText
+
+BOOL ListBoxWindow::Create( HWND hWndParent, HINSTANCE hInstance, LPCTSTR lpszWindowText, DWORD dwExStyle, DWORD dwStyle, HMENU hMenu, int nLeft, int nTop, int nWidth, int nHeight, LPVOID lpParam )
+{
+	BOOL bResult = FALSE;
+
+	// Create window
+	if( Window::Create( LIST_BOX_WINDOW_CLASS_NAME, hWndParent, hInstance, lpszWindowText, dwExStyle, dwStyle, hMenu, nLeft, nTop, nWidth, nHeight, lpParam ) )
+	{
+		// Successfully created window
+
+		// Update return value
+		bResult = TRUE;
+
+	} // End of successfully created windowW
+
+	return bResult;
+
+} // End of function ListBoxWindow::Create
+
+int ListBoxWindow::GetCurrentSelection()
+{
+	// Get current selection
+	return ::SendMessage( m_hWnd, LB_GETCURSEL, ( WPARAM )NULL, ( LPARAM )NULL );
+
+} // End of function ListBoxWindow::GetCurrentSelection
+
+int ListBoxWindow::GetText( int nWhichItem, LPTSTR lpszText )
+{
+	// Get text
+	return ::SendMessage( m_hWnd, LB_GETTEXT, ( WPARAM )nWhichItem, ( LPARAM )lpszText );
+
+} // End of function ListBoxWindow::GetText
+
+
+BOOL ListBoxWindow::HandleCommandMessage( WPARAM wParam, LPARAM, void( *lpSelectionChangedFunction )( LPTSTR lpszItemText ), void( *lpDoubleClickFunction )( LPTSTR lpszItemText ) )
+{
+	BOOL bResult = FALSE;
+
+	// Select list box window notification code
+	switch( HIWORD( wParam ) )
+	{
+		case LBN_SELCHANGE:
+		{
+			// A list box window selection change notification code
+			int nSelectedItem;
+
+			// Allocate string memory
+			LPTSTR lpszSelected = new char[ STRING_LENGTH ];
+
+			// Get selected item
+			nSelectedItem = ::SendMessage( m_hWnd, LB_GETCURSEL, ( WPARAM )NULL, ( LPARAM )NULL );
+
+			// Get selected item text
+			if( ::SendMessage( m_hWnd, LB_GETTEXT, ( WPARAM )nSelectedItem, ( LPARAM )lpszSelected ) )
+			{
+				// Successfully got selected item text
+
+				// Call selection changed function
+				( *lpSelectionChangedFunction )( lpszSelected );
+
+				// Update return value
+				bResult = TRUE;
+
+			} // End of successfully got selected item text
+
+			// Free string memory
+			delete [] lpszSelected;
+
+			// Break out of switch
+			break;
+
+		} // End of a list box window selection change notification code
+		case LBN_DBLCLK:
+		{
+			// A list box window double click notification code
+			int nSelectedItem;
+
+			// Allocate string memory
+			LPTSTR lpszSelected = new char[ STRING_LENGTH ];
+
+			// Get selected item
+			nSelectedItem = ::SendMessage( m_hWnd, LB_GETCURSEL, ( WPARAM )NULL, ( LPARAM )NULL );
+
+			// Get selected item text
+			if( ::SendMessage( m_hWnd, LB_GETTEXT, ( WPARAM )nSelectedItem, ( LPARAM )lpszSelected ) )
+			{
+				// Successfully got selected item text
+
+				// Call double click function
+				( *lpDoubleClickFunction )( lpszSelected );
+
+				// Update return value
+				bResult = TRUE;
+
+			} // End of successfully got selected item text
+
+			// Free string memory
+			delete [] lpszSelected;
+
+			// Break out of switch
+			break;
+
+		} // End of a list box window double click notification code
+		default:
+		{
+			// Default list box window notification code
+
+			// Don't need to do anything here, just return with default result
+
+			// Break out of switch
+			break;
+
+		} // End of default list box window notification code
+
+	}; // End of selection for list box window notification code
+
+	return bResult;
+
+} // End of function ListBoxWindow::HandleCommandMessage
+
+BOOL ListBoxWindow::Move( int nLeft, int nTop, int nWidth, int nHeight, BOOL bRepaint )
+{
+	BOOL bResult = FALSE;
+
+	// Move window
+	if( Window::Move( nLeft, nTop, nWidth, nHeight, bRepaint ) )
+	{
+		// Successfully moved window
+
+		// Update return value
+		bResult = TRUE;
+
+	} // End of successfully moved window
+
+	return bResult;
+
+} // End of function ListBoxWindow::Move
+
+/*
+ListBoxWindow::
+{
+} // End of function ListBoxWindow::
+*/
---
+++ ListBoxWindowClass.h
@@ -0,0 +1,45 @@
+// ListBoxWindowClass.h
+
+#pragma once
+
+#include <windows.h>
+#include <commctrl.h>
+
+#include <windows.h>
+
+#include "WindowClass.h"
+
+#define LIST_BOX_WINDOW_CLASS_NAME												WC_LISTBOX
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_TEXT										NULL
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_MENU										NULL
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_EXTENDED_STYLE							0
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_STYLE										( WS_CHILD | WS_VISIBLE | WS_BORDER | WS_HSCROLL | WS_VSCROLL | LBS_NOINTEGRALHEIGHT | LBS_NOTIFY )
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_LEFT										CW_USEDEFAULT
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_TOP										CW_USEDEFAULT
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_WIDTH										CW_USEDEFAULT
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_HEIGHT									CW_USEDEFAULT
+#define LIST_BOX_WINDOW_CLASS_DEFAULT_LP_PARAM									NULL
+
+class ListBoxWindow : public Window
+{
+public:
+	ListBoxWindow();
+	~ListBoxWindow();
+
+	int AddString( LPCTSTR lpszString );
+
+	int AddText( LPCTSTR lpszText );
+
+	BOOL Create( HWND hWndParent, HINSTANCE hInstance, LPCTSTR lpszWindowText = LIST_BOX_WINDOW_CLASS_DEFAULT_TEXT, DWORD dwExStyle = LIST_BOX_WINDOW_CLASS_DEFAULT_EXTENDED_STYLE, DWORD dwStyle = LIST_BOX_WINDOW_CLASS_DEFAULT_STYLE, HMENU hMenu = LIST_BOX_WINDOW_CLASS_DEFAULT_MENU, int nLeft = LIST_BOX_WINDOW_CLASS_DEFAULT_LEFT, int nTop = LIST_BOX_WINDOW_CLASS_DEFAULT_TOP, int nWidth = LIST_BOX_WINDOW_CLASS_DEFAULT_WIDTH, int nHeight = LIST_BOX_WINDOW_CLASS_DEFAULT_HEIGHT, LPVOID lpParam = LIST_BOX_WINDOW_CLASS_DEFAULT_LP_PARAM );
+
+	int GetCurrentSelection();
+
+	int GetText( int nWhichItem, LPTSTR lpszText );
+
+	BOOL HandleCommandMessage( WPARAM wParam, LPARAM lParam, void( *lpSelectionChangedFunction )( LPTSTR lpszItemText ), void( *lpDoubleClickFunction )( LPTSTR lpszItemText ) );
+
+	BOOL Move( int nLeft, int nTop, int nWidth, int nHeight, BOOL bRepaint = TRUE );
+
+protected:
+
+}; // End of class ListBoxWindow
