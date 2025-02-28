55ed1095a9a3280fb2834a9a9c8d534f92498d86
JimSmith1973 <jimpsmith1973@gmail.com>
2025-02-28 20:32:56 +0000

    Updates
    Added Functions to Add Files to List View, and to Get File Paths

--- FileListViewWindowClass.cpp
+++ FileListViewWindowClass.cpp
@@ -7,12 +7,21 @@ FileListViewWindow::FileListViewWindow()
 	// Initialise member variables
 	::ZeroMemory( &m_hWnd, sizeof( m_hWnd ) );
 
+	// Allocate string memory
+	m_lpszParentFolderPath = new char[ STRING_LENGTH + sizeof( char ) ];
+
+	// Clear parent folder path
+	m_lpszParentFolderPath[ 0 ] = ( char )NULL;
+
 } // End of function FileListViewWindow::FileListViewWindow
  
 FileListViewWindow::~FileListViewWindow()
 {
 	// Clear member variables
 	::ZeroMemory( &m_hWnd, sizeof( m_hWnd ) );
+
+	// Free string memory
+	delete [] m_lpszParentFolderPath;
 
 } // End of function FileListViewWindow::~FileListViewWindow
 
@@ -67,6 +76,122 @@ FileListViewWindow::operator HWND()
 
 } // End of function FileListViewWindow::operator HWND()
 
+int FileListViewWindow::AddFiles( LPCTSTR lpszFolderPath, LPCTSTR lpszFileFilter )
+{
+	int nResult = 0;
+
+	WIN32_FIND_DATA wfd;
+	HANDLE hFind;
+
+	// Allocate string memory
+	LPTSTR lpszFullSearchPattern = new char[ STRING_LENGTH ];
+
+	// Store parent folder path
+	lstrcpy( m_lpszParentFolderPath, lpszFolderPath );
+
+	// Ensure that parent folder path ends with a back-slash
+	if( m_lpszParentFolderPath[ lstrlen( m_lpszParentFolderPath ) - sizeof( char ) ] != ASCII_BACK_SLASH_CHARACTER )
+	{
+		// Parent folder path does not end with a back-slash
+
+		// Append a back-slash onto parent folder path
+		lstrcat( m_lpszParentFolderPath, ASCII_BACK_SLASH_STRING );
+
+	} // End of parent folder path does not end with a back-slash
+
+	// Copy parent folder path into full search pattern
+	lstrcpy( lpszFullSearchPattern, m_lpszParentFolderPath );
+
+	// Append file filter onto full search pattern
+	lstrcat( lpszFullSearchPattern, lpszFileFilter );
+
+	// Find first item
+	hFind = FindFirstFile( lpszFullSearchPattern, &wfd );
+
+	// Ensure that first item was found
+	if( hFind != INVALID_HANDLE_VALUE )
+	{
+		// Successfully found first item
+		LVITEM lvItem;
+		int nItemCount;
+		int nWhichItem;
+		SYSTEMTIME stModified;
+
+		// Clear list view item structure
+		::ZeroMemory( &lvItem, sizeof( lvItem ) );
+
+		// Count items on list view window
+		nItemCount = ::SendMessage( m_hWnd, LVM_GETITEMCOUNT, ( WPARAM )NULL, ( LPARAM )NULL );
+
+		// Initialise list view item structure
+		lvItem.mask			= LVIF_TEXT;
+		lvItem.iItem		= nItemCount;
+		lvItem.cchTextMax	= STRING_LENGTH;
+
+		// Allocate string memory
+		LPTSTR lpszModified = new char[ STRING_LENGTH ];
+
+		// Loop through all items
+		do
+		{
+			// Ensure that found item is a file
+			if( !( wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
+			{
+				// Found item is a file
+
+				// Update list view item structure for file name
+				lvItem.iSubItem		= FILE_LIST_VIEW_WINDOW_CLASS_NAME_COLUMN_ID;
+				lvItem.pszText		= wfd.cFileName;
+
+				// Add file to list view window
+				nWhichItem = ::SendMessage( m_hWnd, LVM_INSERTITEM, ( WPARAM )lvItem.iItem, ( LPARAM )&lvItem );
+
+				// Ensure that file was added to list view window
+				if( nWhichItem >= 0 )
+				{
+					// Successfully added file to list view window
+
+					// Get modified time
+					if( FileTimeToSystemTime( ( &( wfd.ftLastWriteTime ) ), &stModified ) )
+					{
+						// Successfully got modified time
+
+						// Format modified text
+						wsprintf( lpszModified, FILE_LIST_VIEW_WINDOW_CLASS_MODIFIED_TEXT_FORMAT_STRING, stModified.wYear, stModified.wMonth, stModified.wDay, stModified.wHour, stModified.wMinute, stModified.wSecond );
+
+						// Update list view item structure for file
+						lvItem.iSubItem		= FILE_LIST_VIEW_WINDOW_CLASS_MODIFIED_COLUMN_ID;
+						lvItem.pszText		= lpszModified;
+
+						// Add file to list view window
+						::SendMessage( m_hWnd, LVM_SETITEM, ( WPARAM )lvItem.iItem, ( LPARAM )&lvItem );
+
+					} // End of successfully got modified time
+
+					// Update return value
+					nResult ++;
+
+				} // End of successfully added file to list view window
+
+			} // End of found item is a file
+
+		} while( FindNextFile( hFind, &wfd ) ); 
+
+		// Close file find
+		FindClose( hFind );
+
+		// Free string memory
+		delete [] lpszModified;
+
+	} // End of successfully found first item
+
+	// Free string memory
+	delete [] lpszFullSearchPattern;
+
+	return nResult;
+
+} // End of function FileListViewWindow::AddFiles
+
 BOOL FileListViewWindow::Create( HWND hWndParent, HINSTANCE hInstance, LPCTSTR lpszWindowText, HMENU hMenu, DWORD dwExStyle, DWORD dwStyle, int nLeft, int nTop, int nWidth, int nHeight, LPVOID lpParam )
 {
 	BOOL bResult = FALSE;
@@ -94,6 +219,32 @@ BOOL FileListViewWindow::Create( HWND hWndParent, HINSTANCE hInstance, LPCTSTR l
 	return bResult;
 
 } // End of function FileListViewWindow::Create
+
+int FileListViewWindow::GetItemPath( int nWhichItem, LPTSTR lpszItemPath )
+{
+	int nResult;
+
+	LVITEM lvItem;
+
+	// Copy parent folder path into item path
+	lstrcpy( lpszItemPath, m_lpszParentFolderPath );
+
+	// Clear list view item structure
+	::ZeroMemory( &lvItem, sizeof( lvItem ) );
+
+	// Initialise list view item structure
+	lvItem.mask			= LVIF_TEXT;
+	lvItem.iItem		= nWhichItem;
+	lvItem.iSubItem		= FILE_LIST_VIEW_WINDOW_CLASS_NAME_COLUMN_ID;
+	lvItem.pszText		= ( lpszItemPath + lstrlen( lpszItemPath ) );
+	lvItem.cchTextMax	= STRING_LENGTH;
+
+	// Set item text
+	nResult = ::SendMessage( m_hWnd, LVM_GETITEM, ( WPARAM )nWhichItem, ( LPARAM )&lvItem );
+
+	return nResult;
+
+} // End of function FileListViewWindow::GetItemPath
 
 BOOL FileListViewWindow::HandleNotifyMessage( WPARAM, LPARAM lParam, void( *lpSelectionChangedFunction )( LPCTSTR lpszItemText ), void( *lpDoubleClickFunction )( LPCTSTR lpszItemText ), PFNLVCOMPARE lpCompareFunction )
 {
@@ -128,23 +279,23 @@ BOOL FileListViewWindow::HandleNotifyMessage( WPARAM, LPARAM lParam, void( *lpSe
 				// Selection has changed
 
 				// Allocate string memory
-				LPTSTR lpszItemText = new char[ STRING_LENGTH + sizeof( char ) ];
+				LPTSTR lpszItemPath = new char[ STRING_LENGTH + sizeof( char ) ];
 
-				// Get item text
-				if( GetItemText( lpNmListView->iItem, lpNmListView->iSubItem, lpszItemText ) )
+				// Get item path
+				if( GetItemPath( lpNmListView->iItem, lpszItemPath ) )
 				{
-					// Successfully got item text
+					// Successfully got item path
 
 					// Call selection changed function
-					( *lpSelectionChangedFunction )( lpszItemText );
+					( *lpSelectionChangedFunction )( lpszItemPath );
 
 					// Update return value
 					bResult = TRUE;
 
-				} // End of successfully got item text
+				} // End of successfully got item path
 
 				// Free string memory
-				delete [] lpszItemText;
+				delete [] lpszItemPath;
 
 			} // End of selection has changed
 
@@ -162,23 +313,23 @@ BOOL FileListViewWindow::HandleNotifyMessage( WPARAM, LPARAM lParam, void( *lpSe
 				// An item is selected
 
 				// Allocate string memory
-				LPTSTR lpszItemText = new char[ STRING_LENGTH + sizeof( char ) ];
+				LPTSTR lpszItemPath = new char[ STRING_LENGTH + sizeof( char ) ];
 
-				// Get item text
-				if( GetItemText( lpNmListView->iItem, FILE_LIST_VIEW_WINDOW_CLASS_NAME_COLUMN_ID, lpszItemText ) )
+				// Get item path
+				if( GetItemPath( lpNmListView->iItem, lpszItemPath ) )
 				{
-					// Successfully got item text
+					// Successfully got item path
 
 					// Call double click function
-					( *lpDoubleClickFunction )( lpszItemText );
+					( *lpDoubleClickFunction )( lpszItemPath );
 
 					// Update return value
 					bResult = TRUE;
 
-				} // End of successfully got item text
+				} // End of successfully got item path
 
 				// Free string memory
-				delete [] lpszItemText;
+				delete [] lpszItemPath;
 
 			} // End of an item is selected
 
@@ -202,6 +353,23 @@ BOOL FileListViewWindow::HandleNotifyMessage( WPARAM, LPARAM lParam, void( *lpSe
 	return bResult;
 
 } // End of function FileListViewWindow::HandleNotifyMessage
+
+int FileListViewWindow::Populate( LPCTSTR lpszFolderPath, LPCTSTR lpszFileFilter )
+{
+	int nResult;
+
+	// Delete all items from list view window
+	DeleteAllItems();
+
+	// Add files to list view window
+	nResult = AddFiles( lpszFolderPath, lpszFileFilter );
+
+	// Auto-size all list wiew window columns
+	AutoSizeAllColumns();
+
+	return nResult;
+
+} // End of function FileListViewWindow::Populate
 
 /*
 FileListViewWindow::
--- FileListViewWindowClass.h
+++ FileListViewWindowClass.h
@@ -21,6 +21,9 @@
 #define FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_LP_PARAM							NULL
 #define FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_COLUMN_WIDTH						100
 
+#define FILE_LIST_VIEW_WINDOW_CLASS_ADD_FILES_STATUS_MESSAGE_FORMAT_STRING		"%s (%d files)"
+#define FILE_LIST_VIEW_WINDOW_CLASS_MODIFIED_TEXT_FORMAT_STRING					"%04d/%02d/%02d %02d:%02d:%02d"
+
 #define FILE_LIST_VIEW_WINDOW_CLASS_CLASS_COLUMN_TITLES							{ "Name", "Modified" }
 
 typedef enum
@@ -46,10 +49,17 @@ public:
 
 	operator HWND();
 
+	int AddFiles( LPCTSTR lpszFolderPath, LPCTSTR lpszFileFilter );
+
 	BOOL Create( HWND hWndParent, HINSTANCE hInstance, LPCTSTR lpszWindowText = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_TEXT, HMENU hMenu = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_MENU, DWORD dwExStyle = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_EXTENDED_STYLE, DWORD dwStyle = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_STYLE, int nLeft = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_LEFT, int nTop = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_TOP, int nWidth = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_WIDTH, int nHeight = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_HEIGHT, LPVOID lpParam = FILE_LIST_VIEW_WINDOW_CLASS_DEFAULT_LP_PARAM );
+
+	int GetItemPath( int nWhichItem, LPTSTR lpszItemPath );
 
 	BOOL HandleNotifyMessage( WPARAM wParam, LPARAM lParam, void( *lpSelectionChangedFunction )( LPCTSTR lpszItemText ), void( *lpDoubleClickFunction )( LPCTSTR lpszItemText ), PFNLVCOMPARE lpCompareFunction );
 
+	int Populate( LPCTSTR lpszFolderPath, LPCTSTR lpszFileFilter );
+
 protected:
+	LPTSTR m_lpszParentFolderPath;
 
 }; // End of class FileListViewWindow
